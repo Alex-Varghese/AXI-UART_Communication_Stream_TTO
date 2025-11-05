19561ebad9e9162ca30450858d08a19b07075428
Alex Varghese <117454288+Alex-Varghese@users.noreply.github.com>
2025-11-05 10:40:04 +0530

    Refactor package.sv to include additional modules

--- src/package.sv
+++ src/package.sv
@@ -1,21 +1,21 @@
-QuestaSim vlog 10.6c Compiler 2017.07 Jul 25 2017
-Start time: 15:42:04 on Nov 04,2025
-vlog -sv "+acc" "+cover" "+fcover" -l package.sv top.sv 
--- Compiling module uart_rec
--- Compiling module uart_tx
--- Compiling module sync_fifo
--- Compiling module axis_fifo_uart_tx
--- Compiling module axis_master_inp
--- Compiling module top_axis_uart
--- Compiling interface intf
--- Compiling module assertions
--- Compiling package top_sv_unit
--- Importing package mtiUvm.uvm_pkg (uvm-1.1d Built-in)
--- Importing package pkg
--- Compiling module top
+`include "uvm_macros.svh"
+package pkg;
+  int count;
+  reg [10:0] uart_pkt;
+  import uvm_pkg::*;
+  `include "defines.sv"
 
-Top level modules:
-	assertions
-	top
-End time: 15:42:05 on Nov 04,2025, Elapsed time: 0:00:01
-Errors: 0, Warnings: 0
+  `include "seq_item.sv"
+  `include "sequence.sv"
+  `include "sequencer.sv"
+
+  `include "driver.sv"
+  `include "monitor.sv"
+
+  `include "agent.sv"
+
+  `include "scoreboard.sv"
+  `include "coverage.sv"
+  `include "environment.sv"
+  `include "test.sv"
+endpackage
